b9f387344fb21ca20e8bbaae5b9e5b517463d54e
SquadWuschel <joecorporation@gmx.de>
2016-03-30 14:51:19 +0200

    Neues Projket hinzugefügt

---
+++ Angular2Introduction/Angular2Introduction/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Angular2Introduction.MvcApplication" Language="C#" %>
---
+++ Angular2Introduction/Angular2Introduction/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular2Introduction.Startup))]
+namespace Angular2Introduction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
