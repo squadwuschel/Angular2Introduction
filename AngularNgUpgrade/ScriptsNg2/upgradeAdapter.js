2cd6799628c538238461196a2303e7b5d04a63de
squad <squad@SQUADWUSCHEL>
2016-10-20 22:01:31 +0200

    Kommentar

--- AngularNgUpgrade/ScriptsNg2/upgradeAdapter.js
+++ AngularNgUpgrade/ScriptsNg2/upgradeAdapter.js
@@ -1,5 +1,6 @@
 "use strict";
 var upgrade_1 = require('@angular/upgrade');
 var appModule_1 = require('./appModule');
+//Den Upgrade Adapter bereitstellen, damit dieser überall verwendet werden kann.
 exports.upgradeAdapter = new upgrade_1.UpgradeAdapter(appModule_1.AppModule);
 //# sourceMappingURL=upgradeAdapter.js.map
--- AngularNgUpgrade/ScriptsNg2/upgradeAdapter.ts
+++ AngularNgUpgrade/ScriptsNg2/upgradeAdapter.ts
@@ -1,4 +1,5 @@
 ﻿import { UpgradeAdapter } from '@angular/upgrade';
 import { AppModule } from './appModule';
 
+//Den Upgrade Adapter bereitstellen, damit dieser überall verwendet werden kann.
 export const upgradeAdapter = new UpgradeAdapter(AppModule);
