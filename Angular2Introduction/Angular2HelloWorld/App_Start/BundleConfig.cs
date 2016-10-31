8c4aa6261793e8958c652fa68cd253b57d0d8f19
squad <squad@SQUADWUSCHEL>
2016-10-31 12:54:10 +0100

    Hello World simple app

---
+++ Angular2Introduction/Angular2HelloWorld/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Angular2HelloWorld
+{
+    public class BundleConfig
+    {
+        // Weitere Informationen zu Bundling finden Sie unter "http://go.microsoft.com/fwlink/?LinkId=301862"
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Angular2Introduction/Angular2HelloWorld/ScriptsApp/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1 class="well well-sm text-center">Hello World to - {{name}}</h1>'
+})
+export class AppComponent {
+    public name : string = "SquadWuschel";
+
+    constructor() {
+    }
+}
