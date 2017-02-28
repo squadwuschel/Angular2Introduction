3bc462fced08ff12f5b1cb8ed65a29d277e5983d
squad <squad@SQUADWUSCHEL>
2017-02-28 09:28:39 +0100

    typings angepasst

--- Angular2HelloWorld/ScriptsApp/boot.js
+++ Angular2HelloWorld/ScriptsApp/boot.js
@@ -1,5 +1,4 @@
 "use strict";
-/// <reference path="../typings/index.d.ts" />
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require("./app.module");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
